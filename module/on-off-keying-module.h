952ffef8ba4965bf4c537ceb356f4e1b30527142
RyanAckerman <rea9@njit.edu>
2015-04-03 15:07:15 -0400

    Create on-off-keying-module.h

---
+++ module/on-off-keying-module.h
@@ -0,0 +1,11 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef ON_OFF_KEYING_MODULE_H
+#define ON_OFF_KEYING_MODULE_H
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* ON_OFF_KEYING_MODULE_H */
