5c000dc273baf6993b2affe496be2a5f7cb13854
JamesBasuino <jb392@njit.edu>
2015-04-04 13:43:56 -0400

    Update on-off-keying-module.h
    Line 12: do not know if we need an OOK protocol header

--- module/on-off-keying-module.h
+++ module/on-off-keying-module.h
@@ -1,11 +1,14 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
-#ifndef ON_OFF_KEYING_MODULE_H
+
+#ifdef NS3_MODULE_COMPILATION
+# error "Do not include ns3 module aggregator headers from other modules; these are meant only for end user scripts."
+#endif
+
+#ifndef ON_OFF_KEYING_MODULE_H   
+// Module headers:
+#include "on-off-keying-channel.h"
+#include "on-off-keying-helper.h"
+#include "on-off-keying-net-device.h"
+//#include "OOKP-header.h"
 #define ON_OFF_KEYING_MODULE_H
-
-namespace ns3 {
-
-/* ... */
-
-}
-
-#endif /* ON_OFF_KEYING_MODULE_H */
+#endif ON_OFF_KEYING_MODULE_H
