6ddd07f52bb1e563ba787d648642f6a6611e525d
RyanAckerman <rea9@njit.edu>
2015-04-06 23:43:14 -0400

    Update on-off-keying-module-helper.h

--- helper/on-off-keying-module-helper.h
+++ helper/on-off-keying-module-helper.h
@@ -1,1 +1,51 @@
 
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef ON_OFF_KEYING_MODULE_HELPER_H
+#define ON_OFF_KEYING_MODULE_HELPER_H
+
+#include "ns3/on-off-keying-module.h"
+
+namespace ns3 {
+
+class Queue;
+class NetDevice;
+class Node;
+
+class OOKHelper : public PcapHelperForDevice, public AsciiTraceHelperForDevice{
+
+public:
+	OOKHelper();
+	virtual ~OOKHelper(){}
+
+
+	void SetQueue(str::string type, std::string n1= "", const AttributeValue &v1 = EmptyAttributeValue(),
+					std::string n2 = "" ,const AttributeValue &v2 = EmptyAttributeValue(),
+					std::string n3 = "" ,const AttributeValue &v3 = EmptyAttributeValue(),
+					std::string n4 = "", const AttributeValue &v4 = EmptyAttributeValue());
+
+	void SetDeviceAttribute(std::string name, const AttributeValue &value);
+
+	void SetChannelAttribute(std::string name, const AttributeValue &value);
+
+	NetDeviceContainer Install(NodeContainer c);
+
+	NetDeviceContainer Install(Ptr<Node> a, Ptr<Node> b);
+
+	NetDeviceContainer Install(Ptr<Node> a, std::string bname);
+
+	NetDeviceContainer Install(std::string aname, std::string bname);
+
+private:
+	virtual void EnablePcapInternal(std::string prefix, Ptr<NetDevice> nd, bool promiscuous, bool explicitFilname);
+
+	virtual void EnableAsciiInternal(Ptr<OutputStreamWrapper> stream, std::string prefix, Ptr<NetDevice> nd, bool explicitFilename);
+
+	ObjectFactory m_queueFactory;
+	ObjectFactory m_channelFactory;
+	ObjectFactory m_remoteChannelFactory;
+	ObjectFactory m_deviceFactory;
+};
+
+}
+
+#endif /* ON_OFF_KEYING_MODULE_HELPER_H */
