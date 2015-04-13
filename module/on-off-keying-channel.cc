0379e164e5d79c2cf54c39fc8f256a7b82086442
RyanAckerman <rea9@njit.edu>
2015-04-13 00:12:17 -0400

    Update on-off-keying-channel.cc

--- module/on-off-keying-channel.cc
+++ module/on-off-keying-channel.cc
@@ -16,8 +16,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include "point-to-point-channel.h"
-#include "point-to-point-net-device.h"
+#include "on-off-keying-channel.h"
+#include "on-off-keying-net-device.h"
 #include "ns3/trace-source-accessor.h"
 #include "ns3/packet.h"
 #include "ns3/simulator.h"
