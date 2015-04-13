75ed64afda5604ac3f63ff7f74654409715b353f
RyanAckerman <rea9@njit.edu>
2015-04-13 00:20:00 -0400

    Create on-off-keying-remote-channel.h

---
+++ module/on-off-keying-remote-channel.h
@@ -0,0 +1,67 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2007 University of Washington
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: George Riley <riley@ece.gatech.edu>
+ */
+
+
+
+#ifndef ON_OFF_KEYING_REMOTE_CHANNEL_H
+#define ON_OFF_KEYING_REMOTE_CHANNEL_H
+
+#include "on-off-keying-channel.h"
+
+namespace ns3 {
+
+
+class OOKRemoteChannel : public OOKChannel
+{
+public:
+  /**
+   * \brief Get the TypeId
+   *
+   * \return The TypeId for this class
+   */
+  static TypeId GetTypeId (void);
+
+  /** 
+   * \brief Constructor
+   */
+  OOKRemoteChannel ();
+
+  /** 
+   * \brief Deconstructor
+   */
+  ~OOKRemoteChannel ();
+
+  /**
+   * \brief Transmit the packet
+   *
+   * \param p Packet to transmit
+   * \param src Source PointToPointNetDevice
+   * \param txTime Transmit time to apply
+   * \returns true if successful (currently always true)
+   */
+  virtual bool TransmitStart (Ptr<Packet> p, Ptr<OOKNetDevice> src,
+                              Time txTime);
+};
+
+} // namespace ns3
+
+#endif
+
+
