56031b273cdf55cf4f7c1aaba0f3fd40cee56458
RyanAckerman <rea9@njit.edu>
2015-05-28 16:04:33 -0400

    Update Aerror-model.cc

--- model/Aerror-model.cc
+++ model/Aerror-model.cc
@@ -1,8 +1,6 @@
 
 /* 
  *
-/* 
- *
  * Author: Ryan Ackerman   <rea9@njit.edu>
  *         
  *         
@@ -38,8 +36,9 @@ double Eb = 0;
 double Rx = 0;
 double BER = 0;
 double Rb = 0;
-double Res =1; 
-
+double Res =1;
+double Tb = 0; 
+double SNR = 0;
 
 
 AErrorModel::AErrorModel ()
@@ -76,15 +75,15 @@ void AErrorModel::DoReset(void){
 
 double AErrorModel::calculateBER (){
 calculateEb();
-double BER = 0.5*erfc(0.5 *(1/std::sqrt(2))*std::sqrt(Eb/No));
-//std::cout<< " Eb/No : " <<(Eb/No) << std::endl;
+SNR = std::pow((Rx*Res),2)/No;
+double correctSNR = 10 * std::log10(SNR);
+double BER = 0.5*erfc(std::sqrt(correctSNR/2));
+//std::cout<< " SNR : " <<(SNR) << std::endl;
 return BER;
 }
 
 void AErrorModel::calculateEb(){
-        //double Tb = 1/Rb;
-       // Eb = 2 * std::pow(Res*Rx, 2) * Tb;
-	Eb = Rx;
+	//Eb = Rx;
   
 }
 void AErrorModel::setNo (double n){
@@ -97,6 +96,7 @@ Rx = x;
 
 void AErrorModel::setRb (double b){
 Rb =b;
+Tb = 1/Rb;
 }
 
 void AErrorModel::setRes (double r){
@@ -106,86 +106,15 @@ double AErrorModel::getEb(void){
 return Eb;
 }
 
+double AErrorModel::getNo(void){
+return No;
+}
+
 double AErrorModel::getBER(void){
 return BER;
 }
-
-} // namespace ns3
-
- * Author: Ryan Ackerman   <rea9@njit.edu>
- *         
- *         
-*/
-
-#include <cmath>
-
-#include "ns3/Aerror-model.h"
-#include "ns3/packet.h"
-#include "ns3/assert.h"
-#include "ns3/log.h"
-#include "ns3/boolean.h"
-#include "ns3/enum.h"
-#include "ns3/double.h"
-#include "ns3/string.h"
-#include "ns3/pointer.h"
-
-namespace ns3 {
-
-
-//
-// AErrorModel
-//
-NS_LOG_COMPONENT_DEFINE("AErrorModel");
-
-
-NS_OBJECT_ENSURE_REGISTERED (AErrorModel);
-
-double No = 0;
-double Eb = 0;
-
-AErrorModel::AErrorModel ()
-{
-  NS_LOG_FUNCTION (this);
-}
-
-AErrorModel::~AErrorModel () 
-{
-  NS_LOG_FUNCTION (this);
-}
-
-TypeId AErrorModel::GetTypeId (void)
-{ 
-  static TypeId tid = TypeId ("ns3::AErrorModel")
-    .SetGroupName("Network")
-    .AddConstructor<AErrorModel> ()
-    ;
-  return tid;
-}
-
-bool AErrorModel::DoCorrupt(Ptr<Packet> p){
-	NS_LOG_FUNCTION(this << p);
-	double BER = calculateBER();
-	double per = 1.0 - (double)std::pow((double)(1.0 - BER), static_cast<double>(8*p->GetSize()));
-	double rnd  = (double) rand()/(double)(RAND_MAX);
-	return (rnd < per);
-}
-void AErrorModel::DoReset(void){
-
-
-}
-
-double AErrorModel::calculateBER (){
-double BER = 0.5*erfc(std::sqrt(Eb/No));
-//std::cout<<(Eb/No) << std::endl;
-return BER;
-}
-
-void AErrorModel::setNo (double n){
- No = n;
-}
-
-void AErrorModel::setEb (double e){
-Eb = e;
+double AErrorModel::getSNR(void){
+return 10*std::log10(SNR);
 }
 
 } // namespace ns3
