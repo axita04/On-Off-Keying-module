bc006b242f2be08829873b3e3d70bb846b18b7a9
RyanAckerman <rea9@njit.edu>
2015-06-02 12:55:33 -0400

    Update Aerror-model.cc

--- model/Aerror-model.cc
+++ model/Aerror-model.cc
@@ -33,20 +33,19 @@ NS_LOG_COMPONENT_DEFINE("AErrorModel");
 
 NS_OBJECT_ENSURE_REGISTERED (AErrorModel);
 
-double No = 0;
-double Eb = 0;
-double Rx = 0;
-double BER = 0;
-double Rb = 0;
-double Res =1;
-double Tb = 0; 
-double SNR = 0;
+double No = 0;   // Noise power in A^2
+double Rx = 0;   // Received Power in dbm
+double BER = 0;  // Bit Error Rate
+double Rb = 0;   // Data Rate
+double Res =1;   //Responsitivity of Receiver
+double Tb = 0;   // 1 / data rate
+double SNR = 0;  //Signal to noise ratio
 
-
+//Constructor
 AErrorModel::AErrorModel ()
 {
   NS_LOG_FUNCTION (this);
-  srand(time(NULL));
+  srand(time(NULL));  //Seeds Random Number Generator
 }
 
 AErrorModel::~AErrorModel () 
@@ -63,25 +62,29 @@ TypeId AErrorModel::GetTypeId (void)
      ;
   return tid;
 }
-
+// Virtual method from Error Model.  Determinds what packets to be dropped.
 bool AErrorModel::DoCorrupt(Ptr<Packet> p){
 	NS_LOG_FUNCTION(this << p);
+	
 	BER = calculateBER();
+	//Caculated the Packet Error Rate by finding the complement of the probablility 
+	//that a packets is not corrupted
 	double per = 1.0 - (double)std::pow((double)(1.0 - BER), static_cast<double>(8*p->GetSize()));
-        
+        //Randomizies a number and if its less than the PER the packet is rejected
 	double rnd  = (double) rand()/(double)(RAND_MAX);
-
         return (rnd < per);
 }
 void AErrorModel::DoReset(void){
 
 
 }
-
+//Calculates BER from SNR
 double AErrorModel::calculateBER (){
+//SNR calculation	
 SNR = 10 * std::log10(std::pow((Rx*Res),2)/No);
 double BER;
 if(SNR > 0){
+//BER calculation
 BER = 0.5*erfc(std::sqrt(SNR/2));
 }else{
 BER = 1;
@@ -89,38 +92,32 @@ BER = 1;
 
 return BER;
 }
-
-void AErrorModel::calculateEb(){
-	//Eb = Rx;
-  
-}
+//Set Noise power
 void AErrorModel::setNo (double n){
  No = n;
 }
-
+//Set Rx Power
 void AErrorModel::setRx (double x){
 Rx = x;
 }
-
+//Sets bit rate and inverse of bit rate
 void AErrorModel::setRb (double b){
 Rb =b;
 Tb = 1/Rb;
 }
-
+//Sets resposetivity
 void AErrorModel::setRes (double r){
 Res = r;
 }
-double AErrorModel::getEb(void){
-return Eb;
-}
-
+//Gets Noise power
 double AErrorModel::getNo(void){
 return No;
 }
-
+//Gets BER
 double AErrorModel::getBER(void){
 return BER;
 }
+//Gets SNR
 double AErrorModel::getSNR(void){
 return SNR;
 }
