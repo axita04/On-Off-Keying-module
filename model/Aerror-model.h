ef57ccc80dcf6b9cc98bf74e9b2c8438dcc8a1f6
RyanAckerman <rea9@njit.edu>
2015-06-09 15:57:43 -0400

    Update Aerror-model.h

--- model/Aerror-model.h
+++ model/Aerror-model.h
@@ -22,39 +22,47 @@ class AErrorModel : public ErrorModel
 {
 public:
 
-  AErrorModel ();      //Constructor
-   ~AErrorModel ();    //Deconstructor
+  AErrorModel ();
+   ~AErrorModel ();
 
   /**
    * \brief Get the type ID.
    * \return the object TypeId
    */
-  static TypeId GetTypeId (void); //Method for all NS 3 objects sets up traces and such 
-                                  //Not really used for Aerror-model
+  static TypeId GetTypeId (void);
 
-  static double calculateBER (); //Calculate Bit Error Rate
+  static double calculateBER ();
 
-  static void setNo (double n); //sets Noise Power
+  static void calculateEb();
 
-  static void setRx (double x); //sets Received Power
+  static void setNo (double n);
 
-  static void setRb (double b); //Sets bit rate and sets and inverse
+  static void setRx (double x);
 
-  static void setRes (double r); //sets Responcivity 
+  static void setRb (double b);
 
-  static double getBER(void); //return BER
+  static void setRes (double r);
+
+  static double getEb(void);
+
+  static double getBER(void);
   
-  static double getNo(void); //return Noise Power
+  static double getNo(void);
 
-  static double getSNR(void); //return SNR
+  static double getSNR(void);
 
    
 private:
-//Virtual Methods need for Error model
-virtual bool DoCorrupt(Ptr<Packet> p); //Determines what pacekts are corrupt
 
-virtual void DoReset(void); // Not used
+virtual bool DoCorrupt(Ptr<Packet> p);
 
+virtual void DoReset(void);
+
+
+double m_rate;
+
+Ptr<RandomVariableStream> m_ranvar;
+  
 };
 
 
