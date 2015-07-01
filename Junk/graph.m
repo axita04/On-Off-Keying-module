e830f5f3c362c9d134380afdd845ef653466e8ce
wberreta <walterberreta@gmail.com>
2015-07-01 14:57:20 -0400

    Update graph.m

--- Junk/graph.m
+++ Junk/graph.m
@@ -1,3 +1,5 @@
+%This MATLAB code can be used to graph the output of the simulations
+
 load sim.txt;
 
 x = sim(:,2);
@@ -8,3 +10,11 @@ plot(x, y, 'linewidth', 2);
 title('Throughput');
 xlabel('distance [m]');
 ylabel('Throughput');
+
+%in the script used to output data points the following code should be added:
+%     std::ofstream myfile;
+%     myfile.open("sim.txt");
+%     // calculations and other code goes here
+
+%     myfile << throughput << " " << dist << std::endl;
+%     myfile.close();
