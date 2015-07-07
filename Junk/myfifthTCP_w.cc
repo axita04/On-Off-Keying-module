30a9dce1a52ef59302749102cfbf1b5fe4042730
JamesBasuino <jb392@njit.edu>
2015-07-07 12:14:53 -0400

    Update myfifthTCP_w.cc

--- Junk/myfifthTCP_w.cc
+++ Junk/myfifthTCP_w.cc
@@ -20,7 +20,7 @@
 #include "ns3/internet-module.h"
 #include "ns3/on-off-keying-module-helper.h"
 #include "ns3/applications-module.h"
-#include "ns3/Aerror-model.h"
+#include "ns3/OOK-error-model.h"
 #include "ns3/vlc-propagation-loss-model.h"
 #include "ns3/constant-position-mobility-model.h"
 #include "ns3/constant-velocity-mobility-model.h"
@@ -231,8 +231,8 @@ NodeContainer nodes;
 
   a -> SetPosition (Vector (0.0,0.0,0.0));
   b -> SetPosition (Vector (0.0,5.0,0.0));
-  AErrorModel *em2 ;
-  AErrorModel x;
+  OOKErrorModel *em2 ;
+  OOKErrorModel x;
   em2 = &x;
 
   VLCPropagationLossModel VPLM;
