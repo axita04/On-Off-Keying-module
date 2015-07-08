f09d5b4fc0aa3c1d408082b3f8621f04a97a20a5
RyanAckerman <rea9@njit.edu>
2015-07-08 11:07:27 -0400

    Create PAM-error-model.h

---
+++ model/PAM-error-model.h
@@ -0,0 +1,94 @@
+
+#ifndef PAM_ERROR_MODEL_H
+#define PAM_ERROR_MODEL_H
+
+#include <list>
+#include "ns3/object.h"
+#include "ns3/random-variable-stream.h"
+#include "ns3/error-model.h"
+#include "ns3/traced-value.h"
+
+
+namespace ns3 {
+
+class Packet;
+
+
+
+//
+//AErrorModel
+//
+class PAMErrorModel : public ErrorModel
+{
+public:
+
+  PAMErrorModel ();
+   ~PAMErrorModel ();
+
+  /**
+   * \brief Get the type ID.
+   * \return the object TypeId
+   */
+
+  //static TypeId GetTypeId (void);
+
+  double No;   // Noise power in A^2
+  double Rx;   // Received Power in dbm
+  double SER;  // Bit Error Rate
+  double res;  //Responsitivity of Receiver
+  double SNR;  //Signal to noise ratio
+  int wavelength_lower; //Lower bound WaveLength
+  int wavelength_upper; //Upper bound Wavelength
+  double temp;  // Blackbody temp of LED
+  double M; //Size of Symbol
+
+  double calculateSER ();
+
+  void calculateEb();
+
+  double SpectralRadiance(int wavelength, double temperature);
+
+  double integralLum();
+
+  double integralPlanck();
+
+  double integralRes();
+
+  double getWavelengthUpper();
+
+  double getWavelengthLower();
+
+  double getTemperature();
+
+  void setNo (int lower, int upper, int T ,double n, double a);
+
+  void setRx (double x);
+  
+  void setM (double m);
+
+  double getEb(void);
+
+  double getSER(void);
+  
+  double getNo(void);
+
+  double getSNR(void);
+   
+private:
+
+virtual bool DoCorrupt(Ptr<Packet> p);
+
+virtual void DoReset(void);
+
+
+double m_rate;
+
+
+
+Ptr<RandomVariableStream> m_ranvar;
+  
+};
+
+
+} // namespace ns3
+#endif
